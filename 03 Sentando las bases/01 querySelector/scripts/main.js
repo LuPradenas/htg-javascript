b602d8af725835c2febeb15c87734aa7ee513889
lazaronazareno <lucianapradenas3@gmail.com>
2020-01-10 17:09:40 -0300

    realice hasta el ejercicio tres

--- 03 Sentando las bases/01 querySelector/scripts/main.js
+++ 03 Sentando las bases/01 querySelector/scripts/main.js
@@ -1,1 +1,21 @@
 console.info('Cuando veas éste mensaje en consola, ya habrás terminado ésta parte del ejercicio. Borra ésta línea cuando ya la hayas visto alguna vez en la consola al acceder a ésta página.')
+ /* ejercicio 1 */
+let comentario = document.querySelectorAll('.comment--text');
+
+comentario[1].classList.add('commint')
+
+ /* Ejercicio 2 */
+let borde = document.querySelectorAll('hr');
+
+for( let i=0;  i < borde.length; i++){
+
+borde[i].style.borderColor = 'red' ;
+}
+
+  /* Ejercicio 3*/
+ let comentarios = document.querySelectorAll('.comment--text')
+ for(let i= 0;i < comentarios.length;i++){
+     if(comentarios[i].textContent.length > 150){
+         comentarios[i].classList.add('ocultos');
+     
+     }    }
