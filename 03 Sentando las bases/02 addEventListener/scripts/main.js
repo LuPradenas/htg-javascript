74a049b57ba5ed45a7e4a5ed57c0db77d13e25cb
Ezequiiel98 <lucianapradenas3@gmail.com>
2020-01-16 17:18:40 -0300

    agrego ejercicio 3

---
+++ 03 Sentando las bases/02 addEventListener/scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const formulario = document.querySelector('#contacto');
+const inputNombre = document.querySelector('#input-nombre');
+const error = document.querySelector('#mensaje-error');
+const enviado = document.querySelector('#enviado');
+const comentario = document.querySelector('#comentarios');
+const inputSolucion = document.querySelector('#input-solucion');
+
+function recibirNombre(event) {
+  let comentarios = document.createElement('p');
+  event.preventDefault();
+  console.log(event);
+
+  enviado.innerText =' El formulario se ha enviado correctamente.';
+  enviado.style.color = 'purple';
+
+  comentario.innerText = inputNombre.value + ' ' + inputSolucion.value;
+  comentario.appendChild(comentarios)
+
+}
+
+function interceptarNombre(event) {
+  let nombre = event.target.value ;
+  console.log(nombre);
+
+   if(nombre.length <= 4 || nombre.length > 50){
+
+    error.innerText = 'El nombre debe ser entre 5 y 50 caracteres';
+    error.style.color = 'red';
+   }
+   else{
+    error.innerText = '';
+
+   }
+   
+}
+
+formulario.addEventListener('submit', recibirNombre);
+inputNombre.addEventListener('input', interceptarNombre);
+inputSolucion.addEventListener('inpur',recibirNombre);
